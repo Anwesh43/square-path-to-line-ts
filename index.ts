c0a9c3ae5405358bb344a2eb1df1a3f4a9c53bd7
Anwesh43 <anweshthecool0@gmail.com>
2021-05-05 07:31:22 +0530

    adding SquarePathToLine class

--- index.ts
+++ index.ts
@@ -182,3 +182,26 @@ class SPTLNode {
         return this 
     }
 }
+
+class SquarePathToLine {
+
+    curr : SPTLNode = new SPTLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
