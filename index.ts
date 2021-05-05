68d8cbc6c694c4b5ff253c00a829b30f0f183b91
Anwesh43 <anweshthecool0@gmail.com>
2021-05-05 07:25:58 +0530

    adding SPTLNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,45 @@ class Animator {
         }
     }
 }
+
+class SPTLNode {
+
+    next : SPTLNode 
+    prev : SPTLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SPTLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSPTLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SPTLNode {
+        var curr : SPTLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
